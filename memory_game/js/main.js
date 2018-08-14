bc0ab1aeda0efd990b68c71dbc6449d5429a2b66
Chris Ducote <chrisd@slappy.local>
2018-08-14 16:29:23 -0400

    created mo game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,16 @@
-
-//This is javascript
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-//first card
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-//second card
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-//Flip the cards
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-//logic conditions
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
+		alert("You found a match!");} 
 	else {
-		alert("Sorry, try again.")
-	}
-}
+		alert("Sorry, try again.")};}
+var flipCard = function(cardId){
+console.log("User flipped " + cards[cardId]); 
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2) {checkForMatch();}
+
+}; 
+flipCard(0);
+flipCard(2);
